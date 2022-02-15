27eb6ceed923a018ba61af2036a6ad8a3c8c87fd
Aditya simant <adityasimant88@gmail.com>
2022-02-15 19:56:27 +0530

    seton click listner to bootom nav.

--- app/src/main/java/com/example/freedom20/MainActivity.java
+++ app/src/main/java/com/example/freedom20/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.freedom20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.freedom20.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView=findViewById(R.id.readableBottomBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+
+                    case R.id.btnhome:
+                        break;
+                    case R.id.btnexplore:
+                        break;
+                    case R.id.btnpost:
+                        break;
+                    case R.id.btnlogs:
+                        break;
+                    case R.id.btnprofile:
+                        break;
+
+                }
+
+                return false;
+            }
+        });
+
     }
 }
