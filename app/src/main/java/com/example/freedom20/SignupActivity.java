1e32e311e6e701b5463206ef97594a0364db1769
Aditya simant <adityasimant88@gmail.com>
2022-03-09 20:46:03 +0530

    login

--- app/src/main/java/com/example/freedom20/SignupActivity.java
+++ app/src/main/java/com/example/freedom20/SignupActivity.java
@@ -1,5 +1,6 @@
 package com.example.freedom20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,17 +9,37 @@ import android.view.View;
 
 import com.example.freedom20.databinding.ActivityLoginBinding;
 import com.example.freedom20.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.zip.Inflater;
 
 public class SignupActivity extends AppCompatActivity {
     ActivitySignupBinding binding;
+    FirebaseAuth auth ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignupBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
+        auth = FirebaseAuth.getInstance();
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.etEmail.getText().toString();
+                String password = binding.etPassword.getText().toString();
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    }
+                });
+            }
+        });
+
         binding.gotoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/freedom20/zerodha_Fragment.java
+++ app/src/main/java/com/example/freedom20/zerodha_Fragment.java
@@ -2,7 +2,9 @@ package com.example.freedom20;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,10 +17,21 @@ public class zerodha_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+//        Bundle bundle = this.getArguments();
+//        String link = bundle.getString("varsity");
         View view = inflater.inflate(R.layout.fragment_zerodha_, container, false);
-        WebView webView = view.findViewById(R.id.zerodhawebview);
-        webView.loadUrl("https://zerodha.com/varsity/");
-        webView.setWebViewClient(new WebViewController());
+        WebView webView = view.findViewById(R.id.webView);
+        getParentFragmentManager().setFragmentResultListener("dataFromQnA", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String link = result.getString("varsity");
+
+                webView.loadUrl(link);
+                webView.setWebViewClient(new WebViewController());
+            }
+        });
+
+
         return view;
     }
 }
