204f3fb94ba6666b62e8403713c1b0cbdfc74329
Aditya simant <adityasimant88@gmail.com>
2022-03-10 21:51:56 +0530

    login

---
+++ app/src/main/java/com/example/freedom20/Models/User.java
@@ -0,0 +1,44 @@
+package com.example.freedom20.Models;
+
+public class User {
+    private String username,email,name,password;
+
+    public User(String username, String email, String name, String password) {
+        this.username = username;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/com/example/freedom20/SignupActivity.java
+++ app/src/main/java/com/example/freedom20/SignupActivity.java
@@ -6,39 +6,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.freedom20.Models.User;
 import com.example.freedom20.databinding.ActivityLoginBinding;
 import com.example.freedom20.databinding.ActivitySignupBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.zip.Inflater;
 
 public class SignupActivity extends AppCompatActivity {
     ActivitySignupBinding binding;
     FirebaseAuth auth ;
+    FirebaseDatabase database ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         auth = FirebaseAuth.getInstance();
-
+        database = FirebaseDatabase.getInstance();
         binding.btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = binding.etEmail.getText().toString();
-                String password = binding.etPassword.getText().toString();
-                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+//                String email = binding.etEmail.getText().toString();
+//                String password = binding.etPassword.getText().toString();
+                auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(),binding.etPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-
+                        if (task.isSuccessful()){
+                            User user = new User(binding.etUsername.getText().toString(),
+                                    binding.etEmail.getText().toString(),
+                                    binding.etName.getText().toString(),
+                                    binding.etPassword.getText().toString());
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("user").child(id).setValue(user);
+                            Toast.makeText(SignupActivity.this, "User added succesfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(SignupActivity.this, "An error occured ", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
             }
         });
+
 
         binding.gotoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
