93c19c35f83e8389ab59d5ee133c33ba7412b6ce
adityasimant <adityasimant88@gmail.com>
2022-04-17 20:00:01 +0530

    commit

---
+++ app/src/main/java/com/example/freedom20/CommentActivity.java
@@ -0,0 +1,14 @@
+package com.example.freedom20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
