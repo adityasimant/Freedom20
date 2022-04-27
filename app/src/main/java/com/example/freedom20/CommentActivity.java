156730a9cd677db5ebe307c145c52026313b51d3
adityasimant <adityasimant88@gmail.com>
2022-04-27 21:05:07 +0530

    commit

--- app/src/main/java/com/example/freedom20/CommentActivity.java
+++ app/src/main/java/com/example/freedom20/CommentActivity.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.freedom20.Models.Dashboard;
+import com.example.freedom20.Models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class CommentActivity extends AppCompatActivity {
-    ImageView cmtProfilePic;
+    ImageView cmtProfilePic,ImgSend;
     TextView cmtHPost,cmtMPost,cmtAuthor;
     Intent intent;
     String postId,postedBy;
@@ -35,6 +38,7 @@ public class CommentActivity extends AppCompatActivity {
         cmtHPost.findViewById(R.id.commentHpost);
         cmtMPost.findViewById(R.id.commentMpost);
         cmtProfilePic.findViewById(R.id.commentProfilepic);
+        ImgSend = findViewById(R.id.commentSent);
 
         intent = getIntent();
         postId = intent.getStringExtra("postId");
@@ -56,6 +60,29 @@ public class CommentActivity extends AppCompatActivity {
             }
         });
 
-//        database.getReference().child()
+        database.getReference().child("user")
+                .child(postedBy).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                Picasso.get().load(user.getProfile())
+                        .placeholder(R.drawable.ic_img_placeholder)
+                        .into(cmtProfilePic);
+                cmtAuthor.setText("By : " + user.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        ImgSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
